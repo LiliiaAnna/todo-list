d4daa872a6156b183bb1d94723e7e93ab7a46141
koroll <koroll@sheridancollege.ca>
2023-05-22 15:57:39 -0400

    small changes in design

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
 function addTask() {
   if (inputBox.value == "") {
     alert("You should write something.");
   } else {
     let li = document.createElement("li");
-    li.innerHTML = inputBox.value;
+
+    // get due date
+    let dueDate = document.getElementById("due-date").value;
+
+    // format due date if it exists
+    let dueDateText = dueDate
+      ? " (Due: " + new Date(dueDate).toLocaleString() + ")"
+      : "";
+
+    li.innerHTML = inputBox.value + dueDateText;
     listContainer.appendChild(li);
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
@@ -15,10 +25,37 @@ function addTask() {
   saveData();
 }
 
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName == "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName == "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
+
+function removeAllTasks() {
+  listContainer.innerHTML = "";
+  saveData();
+}
+
 function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
 }
+
 function showTask() {
   listContainer.innerHTML = localStorage.getItem("data");
+  //
+  const selectElements = document.querySelectorAll(".task-priority select");
+  selectElements.forEach((selectElement) => {
+    const priority = selectElement.value;
+    const taskItem = selectElement.parentNode.parentNode;
+    taskItem.classList.add(`${priority}-priority`);
+  });
 }
+
 showTask();
