d40b37481a5307da9716321c2b7541bbe32d9079
koroll <koroll@sheridancollege.ca>
2023-05-22 16:02:51 -0400

    removing

--- script.js
+++ script.js
@@ -2,20 +2,13 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
-  if (inputBox.value == "") {
+  if (inputBox.value == '') {
     alert("You should write something.");
   } else {
     let li = document.createElement("li");
 
-    // get due date
-    let dueDate = document.getElementById("due-date").value;
 
-    // format due date if it exists
-    let dueDateText = dueDate
-      ? " (Due: " + new Date(dueDate).toLocaleString() + ")"
-      : "";
-
-    li.innerHTML = inputBox.value + dueDateText;
+    li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
@@ -25,18 +18,18 @@ function addTask() {
   saveData();
 }
 
-listContainer.addEventListener(
-  "click",
-  function (e) {
-    if (e.target.tagName == "LI") {
-      e.target.classList.toggle("checked");
-      saveData();
-    } else if (e.target.tagName == "SPAN") {
-      e.target.parentElement.remove();
+listContainer.addEventListener("click", function(e){
+    if (e.target.tagName =="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
     }
-  },
-  false
-);
+    else if(e.target.tagName == "SPAN"){
+        e.target.parentElement.remove();
+    
+    }
+    }, false);
+
+
 
 function removeAllTasks() {
   listContainer.innerHTML = "";
@@ -47,15 +40,18 @@ function saveData() {
   localStorage.setItem("data", listContainer.innerHTML);
 }
 
+
 function showTask() {
   listContainer.innerHTML = localStorage.getItem("data");
-  //
-  const selectElements = document.querySelectorAll(".task-priority select");
+//
+const selectElements = document.querySelectorAll(".task-priority select");
   selectElements.forEach((selectElement) => {
     const priority = selectElement.value;
     const taskItem = selectElement.parentNode.parentNode;
     taskItem.classList.add(`${priority}-priority`);
   });
 }
+
+
 
 showTask();
